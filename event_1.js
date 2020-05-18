ae3dff92960d6e36713ab20dd2b98cd8298a4aca
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-05-18 16:50:39 +0530

    website for testing

--- event_1.js
+++ event_1.js
@@ -13,18 +13,22 @@
 //     // document.getElementById("eventTitleDetail").innerHTML = response.data[0].title.toString();
 //     // document.getElementById("eventSpeakerDetail").innerHTML = "By "+response.data[0].title.toString();
 //   });
-
-  var settings = {
-    "url": "https://karunyaevent.herokuapp.com/event/listevent",
-    "method": "GET",
-    "timeout": 0,
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    document.getElementById("eventTitleDetail").innerHTML = response.data[0].title.toString();
-    document.getElementById("eventSpeakerDetail").innerHTML = "By "+response.data[0].organizer.toString();
-    document.getElementById("eventDescDetail").innerHTML = response.data[0].discription.toString();
-    $("#youtubeEvent").attr("src", response.data[0].youtubeLive.toString());
-    $("#rocketEvent").attr("src", response.data[0].rocketChat.toString());
-  });
+  if (sessionStorage.getItem('status') != null) { 
+    var settings = {
+        "url": "https://karunyaevent.herokuapp.com/event/listevent",
+        "method": "GET",
+        "timeout": 0,
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        document.getElementById("eventTitleDetail").innerHTML = response.data[0].title.toString();
+        document.getElementById("eventSpeakerDetail").innerHTML = "By "+response.data[0].organizer.toString();
+        document.getElementById("eventDescDetail").innerHTML = response.data[0].discription.toString();
+        $("#youtubeEvent").attr("src", response.data[0].youtubeLive.toString());
+        $("#rocketEvent").attr("src", response.data[0].rocketChat.toString());
+      });
+  }
+  else{
+    document.location = "signin.html";
+  }
--- eventpage.js
+++ eventpage.js
@@ -1,10 +1,15 @@
-  var settings = {
-    "url": "https://karunyaevent.herokuapp.com/event/listevent",
-    "method": "GET",
-    "timeout": 0,
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    document.getElementById("eventTitle").innerHTML = response.data[0].title.toString();
-  });
+  if (sessionStorage.getItem('status') != null) { 
+    var settings = {
+      "url": "https://karunyaevent.herokuapp.com/event/listevent",
+      "method": "GET",
+      "timeout": 0,
+    };
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      document.getElementById("eventTitle").innerHTML = response.data[0].title.toString();
+    });
+  }
+  else{
+    document.location = "signin.html";
+  }
