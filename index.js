1ebd57d042ba77aab6cb4b822b53a4eecf2aba59
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-05-22 10:38:28 +0530

    changes

---
+++ index.js
@@ -0,0 +1,40 @@
+$("#moveAfterAuth").click(function (e) {  
+    e.preventDefault();
+
+    // var emailId = $('#emailUser').val();  
+    // var password = $('#passwordUser').val();  
+    // console.log(emailId);   
+    // console.log(person.emailId);
+    // console.log(person.password);
+    // obj = `{"emailId":"${emailId}","password":"${password}"}`;
+    // raw = `{"emailId":"${emailId}","password":"${password}"}`;
+    // let rawData = raw;
+    // console.log(JSON.stringify(obj));
+    // $.ajax({  
+    //     url: 'https://karunyaevent.herokuapp.com/user/login',  
+    //     type: 'POST', 
+    //     contentType: "application/json; charset=utf-8",
+    //     dataType: 'json',  
+    //     data: JSON.stringify({"emailId":emailId,"password":password}), 
+    //     // headers: {
+    //     //     'Authorization': `Bearer ${data.token}`,
+    //     // }, 
+    //     success: function (data, textStatus, xhr) {  
+    //       console.log(data.message);
+    //       if (data.message == "Auth successful") {
+    //         sessionStorage.setItem('status','loggedIn') 
+    //         document.location = "events.html";
+    //         }
+    
+    //     },  
+    //     error: function (xhr, textStatus, errorThrown) {  
+    //         console.log('Error in Operation');  
+    //     }  
+    // });  
+    if (sessionStorage.getItem('status') != null) { 
+        document.location = "events.html";
+      }
+      else{
+        document.location = "login.html";
+      }
+});
